f423a987f7cb62791c9f46c67bcdcc00f35536e4
bsegvic97 <62608075+bsegvic97@users.noreply.github.com>
2020-05-23 10:30:01 +0200

    Initial commit

--- README.md
+++
@@ -1,2 +0,0 @@
-# 1.jednostovanizad
-Created with CodeSandbox
---
+++ src/index.js
@@ -0,0 +1,16 @@
+import "./styles.css";
+
+document.getElementById("app").innerHTML = `
+<h1>Hello Vanilla!</h1>
+<div>
+  We use the same configuration as Parcel to bundle this sandbox, you can find more
+  info about Parcel 
+  <a href="https://parceljs.org" target="_blank" rel="noopener noreferrer">here</a>.
+</div>
+`;
+var e = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+var f = [];
+for (var i = 0; i < e.length; i++) {
+  if (e[i] % 2 === 0) f.push(e[i]);
+}
+console.log(f);
